b53487272bb1c78a65725e12e84d7d12121c5267
Harley Salas <harley@harleysalas.com>
2022-05-25 23:46:16 +0300

    add react to gatsby-ssr

--- web/gatsby-ssr.js
+++ web/gatsby-ssr.js
@@ -5,6 +5,7 @@
  */
 
 // You can delete this file if you're not using it
+import React from "react";
 const { Provider } = require("./src/context/store");
 
 export const wrapRootElement = ({ element }) => <Provider>{element}</Provider>;
