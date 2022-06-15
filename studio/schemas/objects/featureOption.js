e807f43a349babf2aad4b9a6dec96af16afcebae
Harley Salas <harley@harleysalas.com>
2022-06-15 21:11:50 +0300

    added object type for finishing

---
+++ studio/schemas/objects/featureOption.js
@@ -0,0 +1,17 @@
+export default {
+  name: "featureOption",
+  type: "object",
+  title: "Feature Option",
+  fields: [
+    {
+      name: "title",
+      type: "string",
+      title: "Title",
+    },
+    {
+      name: "description",
+      type: "simplePortableText",
+      title: "Description",
+    },
+  ],
+};
