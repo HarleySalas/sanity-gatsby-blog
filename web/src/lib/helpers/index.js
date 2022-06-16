267cc8ea265210769d96e76e800e2b8e932e77de
Harley Salas <harley@harleysalas.com>
2022-06-16 15:21:43 +0300

    added isBrowser helper

--- web/src/lib/helpers/index.js
+++ web/src/lib/helpers/index.js
@@ -3,3 +3,4 @@ export { default as toRubleFormat } from "./toRubleFormat";
 export { default as correctGrammarFromValue } from "./correctGrammarFromValue";
 export { default as correctGrammarForTwo } from "./correctGrammarForTwo";
 export { default as dynamicSort } from "./dynamicSort";
+export { default as isBrowser } from "./isBrowser";
