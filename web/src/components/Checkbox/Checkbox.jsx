37cc05f7d8750199ac0a3f5b8f64784e996b358a
Harley Salas <harley@harleysalas.com>
2022-06-20 19:24:22 +0300

    quick checkbox variant for projectFinish

--- web/src/components/Checkbox/Checkbox.jsx
+++ web/src/components/Checkbox/Checkbox.jsx
@@ -2,19 +2,24 @@ import React, { forwardRef } from "react";
 import "./checkbox.scss";
 
 const Checkbox = forwardRef((props, ref) => {
-  const { checked, disabled, onChange, value } = props;
+  const { checked, disabled, onChange, value, projectStyles } = props;
+
   return (
     <div className="checkbox">
       <label className="checkbox__label">
         <input
           type="checkbox"
           checked={checked}
-          className="checkbox__input"
+          className={`checkbox__input`}
           onChange={onChange}
           value={value}
           disabled={disabled}
         />
-        <span className="checkbox__checkmark"></span>
+        <span
+          className={`checkbox__checkmark ${
+            projectStyles ? "checkbox__checkmark--project" : null
+          }`}
+        ></span>
       </label>
     </div>
   );
